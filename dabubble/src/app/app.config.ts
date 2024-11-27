8639d5b1abb0ef88a808ee9be36539a9c7d18e23
Stefan Kondratschin <kondratschin@gmail.com>
2024-11-27 21:04:10 +0100

    firebase connected

--- dabubble/src/app/app.config.ts
+++ dabubble/src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"dabubble385","appId":"1:571622353144:web:755b346000e1c75f1db30d","storageBucket":"dabubble385.firebasestorage.app","apiKey":"AIzaSyBO5-RemTjbcRntX6acn00Cui941XoJ5cA","authDomain":"dabubble385.firebaseapp.com","messagingSenderId":"571622353144"})), provideFirestore(() => getFirestore())]
 };
