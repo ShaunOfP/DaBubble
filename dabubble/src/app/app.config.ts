60e5f82427de5591a7316c216110532a855f9eae
Stefan Kondratschin <kondratschin@gmail.com>
2024-11-27 21:16:14 +0100

    prettier

--- dabubble/src/app/app.config.ts
+++ dabubble/src/app/app.config.ts
@@ -6,5 +6,19 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"dabubble385","appId":"1:571622353144:web:755b346000e1c75f1db30d","storageBucket":"dabubble385.firebasestorage.app","apiKey":"AIzaSyBO5-RemTjbcRntX6acn00Cui941XoJ5cA","authDomain":"dabubble385.firebaseapp.com","messagingSenderId":"571622353144"})), provideFirestore(() => getFirestore())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'dabubble385',
+        appId: '1:571622353144:web:755b346000e1c75f1db30d',
+        storageBucket: 'dabubble385.firebasestorage.app',
+        apiKey: 'AIzaSyBO5-RemTjbcRntX6acn00Cui941XoJ5cA',
+        authDomain: 'dabubble385.firebaseapp.com',
+        messagingSenderId: '571622353144',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
 };
