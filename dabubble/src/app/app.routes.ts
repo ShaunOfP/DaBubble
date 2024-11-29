f37db30942f92ab2d9b3612ce0c236806a01942a
Philipp Lötszch <philipp.loetzsch@outlook.com>
2024-11-29 14:35:16 +0100

    style buttons and route components

--- dabubble/src/app/app.routes.ts
+++ dabubble/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { GeneralViewComponent } from './components/general-view/general-view.component';
+import { ImprintComponent } from './components/imprint/imprint.component';
+import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginComponent },
+    { path: 'general', component: GeneralViewComponent },
+    { path: 'imprint', component: ImprintComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent },
+];
