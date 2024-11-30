a1f224bc55ad68a58e4b7a3f266771a11a1df999
Isabell Diterle <160329592+Tr0vat@users.noreply.github.com>
2024-11-30 23:12:12 +0100

    styling-sign-in-card

--- dabubble/src/app/components/landing-page/sign-in/sign-in.component.ts
+++ dabubble/src/app/components/landing-page/sign-in/sign-in.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, FormsModule, MatCheckboxModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
 export class SignInComponent {
+name = "";
+mail = "";
+password = "";
 
 }
