4b6b73f720f2566799366229dfbcc8ec4cb5174f
Philipp Lötszch <philipp.loetzsch@outlook.com>
2024-11-30 14:22:24 +0100

    create templates and separate files

--- dabubble/src/app/app.routes.ts
+++ dabubble/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './components/landing-page/login/login.component';
 import { GeneralViewComponent } from './components/general-view/general-view.component';
 import { ImprintComponent } from './components/imprint/imprint.component';
 import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
---
+++ dabubble/src/app/components/landing-page/sing-in/sing-in.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sing-in',
+  standalone: true,
+  imports: [],
+  templateUrl: './sing-in.component.html',
+  styleUrl: './sing-in.component.scss'
+})
+export class SingInComponent {
+
+}
