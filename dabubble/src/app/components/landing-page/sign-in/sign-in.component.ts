000a6c7fc33aeb0894e36dba485fff3e74070437
Isabell Diterle <160329592+Tr0vat@users.noreply.github.com>
2024-12-01 16:52:23 +0100

    including form sign-in

--- dabubble/src/app/components/landing-page/sign-in/sign-in.component.ts
+++ dabubble/src/app/components/landing-page/sign-in/sign-in.component.ts
@@ -1,18 +1,56 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+
+interface InputField {
+  placeholder: string;
+  icon: string;
+  value: string;
+  focused: boolean;
+  type: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [MatCardModule, FormsModule, MatCheckboxModule],
+  imports: [CommonModule ,MatCardModule, FormsModule, MatCheckboxModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
 export class SignInComponent {
-name = "";
-mail = "";
-password = "";
+
+  accountData = {
+    name: "",
+    mail: "",
+    password: "",
+  }
+
+  nameFocused: boolean = false;
+  mailFocused: boolean = false;
+  passwordFocused: boolean = false;
+
+  onFocus(currentState: boolean, field: string){
+    switch (field) {
+      case 'name':
+        this.nameFocused = currentState;
+        break;
+      case 'mail':
+        this.mailFocused = currentState;
+        break;
+      case 'password':
+        this.passwordFocused = currentState;
+        break;
+    }
+  }
+
+  onSubmit(ngForm: NgForm){
+    if(ngForm.valid && ngForm.submitted){
+      console.log(this.accountData)
+    }
+  }
 
 }
+
