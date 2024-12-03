57bf74895605f4c1c1477e87ed477ee1df391ab3
Philipp Lötzsch <philipp.loetzsch@outlook.com>
2024-12-03 19:10:10 +0100

    implemet logic reset pw

--- dabubble/src/app/components/landing-page/reset-password/reset-password.component.ts
+++ dabubble/src/app/components/landing-page/reset-password/reset-password.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule, NgForm} from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { RouterModule } from '@angular/router';
@@ -8,18 +8,28 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-reset-password',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatCardModule, MatInputModule, RouterModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    RouterModule,
+  ],
   templateUrl: './reset-password.component.html',
-  styleUrl: './reset-password.component.scss'
+  styleUrl: './reset-password.component.scss',
 })
 export class ResetPasswordComponent {
-  confirmPassword = {
+  Passwords = {
     newPassword: "",
     confirmedPassword: "",
-  }
-  onSubmit(ngForm: NgForm){
-    if(ngForm.valid && ngForm.submitted){ console.log('neues Passwort:' , this.confirmPassword.newPassword);}
-   
-    
+  };
+  onSubmit(ngForm: NgForm) {
+    if (
+      ngForm.valid &&
+      ngForm.submitted &&
+      this.Passwords.newPassword === this.Passwords.confirmedPassword
+    ) {
+      console.log('neues Passwort:', this.Passwords.newPassword);
+    }
   }
 }
