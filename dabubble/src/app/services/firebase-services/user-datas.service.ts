1ac859f4eb3a322036eea239c3fcee58f51d1202
Philipp Lötzsch <philipp.loetzsch@outlook.com>
2024-12-05 13:05:10 +0100

    change get user function in service

--- dabubble/src/app/services/firebase-services/user-datas.service.ts
+++ dabubble/src/app/services/firebase-services/user-datas.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { inject } from '@angular/core';
 import { Firestore, collection, collectionData, addDoc, getDoc, doc, onSnapshot } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { elementAt, Observable } from 'rxjs';
 import { User } from './../../models/user.class';
 
 
@@ -11,24 +11,24 @@ import { User } from './../../models/user.class';
 export class UserDatasService {
   private firestore = inject(Firestore);
   userDatas$: Observable<User[]>;
+  userDatas:any;
 
   constructor() {
-    const aCollection = collection(this.firestore, 'userDatas');
-    this.userDatas$ = collectionData(aCollection);
+    this.userDatas$ = collectionData(this.userDatasRef());
     this.getUserDatas()
   }
 
   async saveUser(user: User): Promise<void> {
     try {
       const userPlainObject = { ...user }; // Important!! Firebase need a plainobject to read otherwise its not working!
-      const docRef = await addDoc(collection(this.firestore, 'userDatas'), userPlainObject);
+      const docRef = await addDoc(this.userDatasRef(), userPlainObject);
       console.log("Document written with ID: ", docRef.id);
     } catch (err) {
       console.error("Error adding document: ", err);
     }
   }
 
-  async getUserDatas(){  
+/*   async getUserDatas(){  
     const docRef = doc(this.firestore, 'userDatas', 'HWRU8fpwhZTUX6A7NFAa');
     const docSnap = await getDoc(docRef);
     
@@ -36,7 +36,20 @@ export class UserDatasService {
       console.log("Document data:", docSnap.data());
     } else {
       console.log("No such document!");
-    }}
+    }} */
+
+    async getUserDatas(){
+      this.userDatas = this.userDatas$.subscribe( (list) => {
+        list.forEach(element => {
+          console.log(element);
+          
+        })
+      })
+    }
+
+    userDatasRef(){
+      return collection(this.firestore, 'userDatas')
+    }
 }
 
 
