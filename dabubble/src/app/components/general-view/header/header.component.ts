1ae9ba1489921c6678f4c3f093fd1fd4b4c3a584
ShaunOfP <131476113+ShaunOfP@users.noreply.github.com>
2024-12-06 17:57:51 +0100

    added logic for the dropdown menu; also added JSDoc

--- dabubble/src/app/components/general-view/header/header.component.ts
+++ dabubble/src/app/components/general-view/header/header.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
+import { MatMenu, MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
 import { Router, RouterModule } from '@angular/router';
 
 @Component({
@@ -15,11 +15,29 @@ export class HeaderComponent {
 
   }
 
-  activate(){
-    document.getElementById('dropdown-list')?.style.display == 'inline-block';
-  }
+  @ViewChild('menu') menu!: MatMenu;
+  @ViewChild('menuTrigger') menuTrigger!: MatMenuTrigger;
 
+
+  /**
+   * Navigating to a specific site via the router using a given route
+   * @param route A string containing the chosen route
+   */
   navigateTo(route: string) {
     this.router.navigate([route]);
+  }
+
+
+  /**
+   * Toggles the grey screen for the dropdown menu, also closes the menu if the greyScreen is no longer displayed
+   */
+  toggleGreyScreen() {
+    let element = document.getElementById('grey-screen');
+
+    element?.classList.toggle('d-none');
+
+    if (element?.classList.contains('d-none')) {
+      this.menuTrigger.closeMenu();
+    }
   }
 }
