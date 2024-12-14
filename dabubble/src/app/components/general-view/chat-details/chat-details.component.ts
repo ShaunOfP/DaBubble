b9cc2b49dd725086972bd439637f85bbf66611ac
ShaunOfP <131476113+ShaunOfP@users.noreply.github.com>
2024-12-14 15:34:42 +0100

    added styling and logic to open/close the edit container

--- dabubble/src/app/components/general-view/chat-details/chat-details.component.ts
+++ dabubble/src/app/components/general-view/chat-details/chat-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, viewChild, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-chat-details',
@@ -9,8 +9,36 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class ChatDetailsComponent {
   @Output() callParent: EventEmitter<void> = new EventEmitter();
+  @ViewChild('newChannelInputContainer') newInput!: ElementRef;
+  @ViewChild('channelNameEditContainer') channelNameContainer! : ElementRef;
+  @ViewChild('descpritionContainer') descpritionContainer! : ElementRef;
+  @ViewChild('editDescriptionContainer') editDescriptionContainer!: ElementRef;
 
   closeChatDetails() {
     this.callParent.emit();
+  }
+
+  openNewChannelInput(){
+    this.newInput.nativeElement.classList.remove('d-none');
+    this.closeEditChannelContainer();
+  }
+
+  closeEditChannelContainer(){
+    this.channelNameContainer.nativeElement.classList.add('d-none');
+  }
+
+  saveChanges(){
+    this.channelNameContainer.nativeElement.classList.remove('d-none');
+    this.newInput.nativeElement.classList.add('d-none');
+  }
+
+  openDesciptionEdit(){
+    this.descpritionContainer.nativeElement.classList.add('d-none');
+    this.editDescriptionContainer.nativeElement.classList.remove('d-none');
+  }
+
+  saveDescChanges(){
+    this.descpritionContainer.nativeElement.classList.remove('d-none');
+    this.editDescriptionContainer.nativeElement.classList.add('d-none');
   }
 }
