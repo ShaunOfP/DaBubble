13074e097b23e358c694d2fe66ac1459106745e5
Isabell Diterle <160329592+Tr0vat@users.noreply.github.com>
2024-12-14 18:19:24 +0100

    responsivness privacy policy

--- dabubble/src/app/components/privacy-policy/privacy-policy.component.ts
+++ dabubble/src/app/components/privacy-policy/privacy-policy.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
 import { HeaderSectionComponent } from "../landing-page/header-section/header-section.component";
-import { FooterComponent } from "../landing-page/footer/footer.component";
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [MatCardModule, RouterModule, HeaderSectionComponent, FooterComponent],
+  imports: [MatCardModule, RouterModule, HeaderSectionComponent],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
