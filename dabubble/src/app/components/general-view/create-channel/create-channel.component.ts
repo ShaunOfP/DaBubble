e1743114d3eb32b402ef9ddb55f062f050da9625
ShaunOfP <131476113+ShaunOfP@users.noreply.github.com>
2024-12-19 19:29:26 +0100

    added JSDoc

--- dabubble/src/app/components/general-view/create-channel/create-channel.component.ts
+++ dabubble/src/app/components/general-view/create-channel/create-channel.component.ts
@@ -15,42 +15,78 @@ export class CreateChannelComponent {
   newChannelName: string = '';
   newChannelDescription: string | null = '';
 
+
+  /**
+   * Sends a call to the parent component to close the Component
+   */
   closeCreateChannel() {
     this.callParentToClose.emit();
   }
 
+
+  /**
+   * Calls functions to ensure the right classes on the HTMLElements when the Component was closed
+   */
   closeCreateChannelAndChangeClasses() {
     this.closeCreateChannel();
     this.closeAddMembersToNewChannelMenu();
     this.openCreateChannelContainer();
   }
 
+
+  /**
+   * Removes a class to make the AddMembersToNewChannel Component visible
+   */
   openAddMembersToNewChannelMenu() {
     document.getElementById('addMembersToNewChannel')?.classList.remove('d-none');
   }
 
+
+  /**
+   * Adds a class to hide the Create-Channel-Container
+   */
   closeCreateChannelContainer() {
     document.getElementById('create-channel-container')?.classList.add('d-none');
   }
 
+
+  /**
+   * Adds a class to hide the AddMembersToNewChannel Component
+   */
   closeAddMembersToNewChannelMenu() {
     document.getElementById('addMembersToNewChannel')?.classList.add('d-none');
   }
 
+
+  /**
+   * Removes a class to make the Create-Channel-Container visible
+   */
   openCreateChannelContainer() {
     document.getElementById('create-channel-container')?.classList.remove('d-none');
   }
 
+
+  /**
+   * Changes the active class on the submit button to enable it
+   */
   enableSubmitButton() {
     document.getElementById('submit-btn')?.classList.remove('btn-primary--disable');
     document.getElementById('submit-btn')?.classList.add('btn-primary--default');
   }
 
+
+  /**
+   * Changes the active class on the submit button to disable it
+   */
   disableSubmitButton() {
     document.getElementById('submit-btn')?.classList.add('btn-primary--disable');
     document.getElementById('submit-btn')?.classList.remove('btn-primary--default');
   }
 
+
+  /**
+   * Checks, if there is any input in the newChannelName Input Element
+   */
   checkInput() {
     if (this.newChannelName.length > 0) {
       this.enableSubmitButton();
@@ -59,6 +95,11 @@ export class CreateChannelComponent {
     }
   }
 
+
+  /**
+   * 
+   * @param ngForm 
+   */
   formSubmit(ngForm: NgForm) {
     this.openAddMembersToNewChannelMenu();
     this.closeCreateChannelContainer();
