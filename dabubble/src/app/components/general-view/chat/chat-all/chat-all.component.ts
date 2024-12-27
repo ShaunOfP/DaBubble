c3d36221a7368cfeb6cd83a28ad22fd75f2e52a9
Philipp Lötszch <philipp.loetzsch@outlook.com>
2024-12-27 20:38:08 +0100

    show posted time

--- dabubble/src/app/components/general-view/chat/chat-all/chat-all.component.ts
+++ dabubble/src/app/components/general-view/chat/chat-all/chat-all.component.ts
@@ -8,16 +8,25 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './chat-all.component.html',
-  styleUrls: ['./chat-all.component.scss']
+  styleUrls: ['./chat-all.component.scss'],
 })
 export class ChatAllComponent implements OnInit {
   messages$!: Observable<any[]>;
-  threadMessages: any[] = [];
+  threadMessages: string[] = [];
+  date: string[] = [];
 
-  constructor(private chatService: ChatService) { }
+  constructor(private chatService: ChatService) {}
 
   ngOnInit(): void {
     const channelId = 'dOCTHJxiNDhYvmqMokLv'; // Replace with your channel ID
     this.messages$ = this.chatService.getMessages(channelId);
   }
+  formatTime(timestamp: string): string {
+    const parsedTimestamp = parseInt(timestamp, 10);
+    const timestampInMs = parsedTimestamp < 1e12 ? parsedTimestamp * 1000 : parsedTimestamp;
+    const date = new Date(timestampInMs);
+    return date.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' });
+  }
+  
+  
 }
