0321438bdae6399531f87705d3c62775287ac2ff
ShaunOfP <131476113+ShaunOfP@users.noreply.github.com>
2024-12-29 18:25:51 +0100

    added reaction menu logic and emoji picker plus styling

--- dabubble/src/app/components/general-view/thread/thread-messages/thread-messages.component.ts
+++ dabubble/src/app/components/general-view/thread/thread-messages/thread-messages.component.ts
@@ -1,24 +1,50 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ChatService } from '../../../../services/firebase-services/chat.service';
+import { EmojiPickerComponent } from "../../emoji-picker/emoji-picker.component";
 
 @Component({
   selector: 'app-thread-messages',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, EmojiPickerComponent],
   templateUrl: './thread-messages.component.html',
   styleUrl: './thread-messages.component.scss'
 })
 export class ThreadMessagesComponent {
   messages$!: Observable<any[]>;
   showReaction: boolean = true;
+  showEmojiPicker: boolean = false;
+  @ViewChild('emojiTarget', { static: true }) emojiTarget!: ElementRef;
+  selectedEmoji: string = '';
+  reactions: any[] = [{ //type zu object wenn sender berücksichtigt wird
+    'emoji': '😁',
+    'sender': 'Jake'
+  }];
 
   constructor(private chatService: ChatService) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.messages$ = this.chatService.getThreadCollection('dOCTHJxiNDhYvmqMokLv', 'buM6uSAhw8snf948FEIh');
   }
 
   //wenn reaktionen vorhanden dann showReactions auf true
+
+  openEmojiPicker() {
+    this.showEmojiPicker = true;
+  }
+
+  onEmojiReceived(emoji: string) {
+    this.selectedEmoji = emoji;
+    this.reactions.push({
+      'emoji': emoji,
+      'sender': 'Peter'
+    });
+    console.log(this.reactions);
+    this.hideEmojiPicker();
+  }
+
+  hideEmojiPicker() {
+    this.showEmojiPicker = false;
+  }
 }
