e7c47c63bf975b098f0cd16c28d9db3eac571a5f
Philipp Lötszch <philipp.loetzsch@outlook.com>
2025-01-01 20:55:14 +0100

    show only one date of daily conversation

--- dabubble/src/app/components/general-view/chat/chat-all/chat-all.component.ts
+++ dabubble/src/app/components/general-view/chat/chat-all/chat-all.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ChatService } from '../../../../services/firebase-services/chat.service';
-import { Observable, map } from 'rxjs';
+import { Observable, last, map } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { Message } from '../../../../models/interfaces';
 
@@ -20,30 +20,28 @@ export class ChatAllComponent implements OnInit {
   ngOnInit(): void {
     this.messages$ = this.chatService.getMessages(this.channelId).pipe(
       map((messages: Message[]) => {
-        let lastTimestamp = 0;
-
-        return messages.map((message) => {
-          const showDate =
-            lastTimestamp === 0 || lastTimestamp < message.createdAt;
-
-          if (showDate) {
-            lastTimestamp = message.createdAt;
-          }
-
-          return {
-            ...message,
-            showDate,
-            formattedDate: showDate
-              ? new Date(message.createdAt).toLocaleDateString('de-DE', {
-                  day: '2-digit',
-                  month: '2-digit',
-                  year: '2-digit',
-                })
-              : null,
-          };
-        });
+        let lastDate: string | null = null;
+        return this.returnNewObservable(messages, lastDate)
+       
       })
     );
+  }
+  
+  returnNewObservable(messages: Message[], lastDate: string | null){
+    return messages.map((message) => {
+      const currentDate = new Date(message.createdAt).toLocaleDateString('de-DE', {
+        day: '2-digit',
+        month: '2-digit',
+        year: '2-digit',
+      });
+      const showDate = currentDate !== lastDate;
+      lastDate = currentDate; 
+      return {
+        ...message,
+        showDate,
+        formattedDate: showDate ? currentDate : null,
+      };
+    });
   }
 
   formatTime(timestamp: number): string {
