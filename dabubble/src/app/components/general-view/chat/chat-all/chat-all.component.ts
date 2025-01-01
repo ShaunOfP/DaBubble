1fad1d2fa0764df806cf6b94a5bfa0f004576bc2
Philipp Lötszch <philipp.loetzsch@outlook.com>
2025-01-01 14:40:51 +0100

    update chat timstamp

--- dabubble/src/app/components/general-view/chat/chat-all/chat-all.component.ts
+++ dabubble/src/app/components/general-view/chat/chat-all/chat-all.component.ts
@@ -13,7 +13,8 @@ import { Message } from '../../../../models/interfaces';
 })
 export class ChatAllComponent implements OnInit {
   messages$!: Observable<Message[]>;
-  channelId = 'dOCTHJxiNDhYvmqMokLv';
+  channelId: string = 'dOCTHJxiNDhYvmqMokLv';
+  lastTimestamp: string = '';
 
   constructor(private chatService: ChatService) {}
 
@@ -21,15 +22,46 @@ export class ChatAllComponent implements OnInit {
     this.messages$ = this.chatService.getMessages(this.channelId);
   }
 
-  formatTime(timestamp: string): string {
+  parseTimestamp(timestamp:string){
     const parsedTimestamp = parseInt(timestamp, 10);
     const timestampInMs = parsedTimestamp < 1e12 ? parsedTimestamp * 1000 : parsedTimestamp;
+    return timestampInMs
+  }
+
+  formatTime(timestamp: string) {
+    const timestampInMs = this.parseTimestamp(timestamp) 
     const date = new Date(timestampInMs);
     return date.toLocaleTimeString('de-DE', {
       hour: '2-digit',
       minute: '2-digit',
     });
   }
+
+  newDate(timestamp:string){
+    const dateTime = this.parseTimestamp(timestamp)
+    const date = new Date(dateTime).toLocaleDateString('de-DE', {
+      day: '2-digit',
+      month:'2-digit',
+      year: '2-digit',
+    })
+    console.log(date)
+    if(this.lastTimestamp === '' || this.lastTimestamp <= date)
+    {
+      this.lastTimestamp = date
+      return true
+    }
+    else{ return false}
+  }
+
+  // formatDate(timestamp: string){
+  //   const dateTime = this.parseTimestamp(timestamp) 
+  //   const date = new Date(dateTime)
+  //   return date.toLocaleTimeString('de-DE', {
+  //     day: '2-digit',
+  //     month:'2-digit',
+  //     year: '2-digit',
+  //   })
+  // }
 
   checkStyle(userId: string) {
     return userId === 'OI28qbeslOMfD5nSoQMw8vmU6uQ2' ? 'primary' : 'secondary';
