891f0c4083b02cb9277be5a1785f18d908d12811
Philipp Lötszch <philipp.loetzsch@outlook.com>
2025-01-02 20:13:07 +0100

    scroll down when reload

--- dabubble/src/app/components/general-view/chat/chat-all/chat-all.component.ts
+++ dabubble/src/app/components/general-view/chat/chat-all/chat-all.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { ChatService } from '../../../../services/firebase-services/chat.service';
-import { Observable, map } from 'rxjs';
+import { Observable, map, timeout } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { Message } from '../../../../models/interfaces';
 
@@ -23,10 +23,14 @@ export class ChatAllComponent implements OnInit{
   ngOnInit(): void {
     this.messages$ = this.chatService.getMessages(this.channelId).pipe(
       map((messages: Message[]) => {
-        let lastDate: string | null = null;
-        return this.returnNewObservable(messages, lastDate);
+              return this.returnNewObservable(messages, null);
       })
     );
+    setTimeout(() => {
+      this.scrollToElement()
+    }, 1000);
+    
+    
   }
 
   scrollToElement(): void {
