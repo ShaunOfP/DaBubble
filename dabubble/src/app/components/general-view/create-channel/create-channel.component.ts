76813e51efd4766a594d796f663110b675a2111a
Isabell Diterle <160329592+Tr0vat@users.noreply.github.com>
2025-01-25 20:38:47 +0100

    add mobile detection function

--- dabubble/src/app/components/general-view/create-channel/create-channel.component.ts
+++ dabubble/src/app/components/general-view/create-channel/create-channel.component.ts
@@ -1,27 +1,51 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 import { ChannelMemberService } from '../../../services/firebase-services/channel-member.service';
 import { AddMembersToNewChannelComponent } from '../workspace-menu/add-members-to-new-channel/add-members-to-new-channel.component';
-
 
 @Component({
   selector: 'app-create-channel',
   standalone: true,
   imports: [FormsModule, CommonModule, AddMembersToNewChannelComponent],
   templateUrl: './create-channel.component.html',
-  styleUrl: './create-channel.component.scss'
+  styleUrl: './create-channel.component.scss',
 })
-export class CreateChannelComponent {
+export class CreateChannelComponent implements OnInit {
   @Output() callParentToClose: EventEmitter<void> = new EventEmitter();
-  
+
   newChannelName: string = '';
-  newChannelDescription: string  = '';
+  newChannelDescription: string = '';
   createChannelContainerVisible: boolean = true;
   addMembersToNewChannelVisible: boolean = false;
+  isMobile: boolean = false;
 
-  constructor(private memberService: ChannelMemberService,){}
+  constructor(private memberService: ChannelMemberService) {}
+
+  /**
+   * Checks if the user is accessing the page on a mobile device
+   */
+  ngOnInit(): void {
+    const isMobile =
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      );
+    if (isMobile) {
+      console.log('Mobile device detected');
+      this.isMobile = true;
+    } else {
+      console.log('Desktop device detected');
+      this.isMobile = false;
+    }
+  }
 
   /**
    * Sends a call to the parent component to close the Component
@@ -29,7 +53,6 @@ export class CreateChannelComponent {
   closeCreateChannel() {
     this.callParentToClose.emit();
   }
-
 
   /**
    * Calls functions to ensure the right classes on the HTMLElements when the Component was closed
@@ -40,14 +63,12 @@ export class CreateChannelComponent {
     this.openCreateChannelContainer();
   }
 
-
   /**
    * Removes a class to make the AddMembersToNewChannel Component visible
    */
   openAddMembersToNewChannelMenu() {
     this.addMembersToNewChannelVisible = true;
   }
-
 
   /**
    * Adds a class to hide the Create-Channel-Container
@@ -56,14 +77,12 @@ export class CreateChannelComponent {
     this.createChannelContainerVisible = false;
   }
 
-
   /**
    * Adds a class to hide the AddMembersToNewChannel Component
    */
   closeAddMembersToNewChannelMenu() {
     this.addMembersToNewChannelVisible = false;
   }
-
 
   /**
    * Removes a class to make the Create-Channel-Container visible
@@ -72,17 +91,20 @@ export class CreateChannelComponent {
     this.createChannelContainerVisible = true;
   }
 
-
   /**
-   * 
-   * @param ngForm 
+   *
+   *
    */
   formSubmit(ngForm: NgForm) {
     this.openAddMembersToNewChannelMenu();
-    this.closeCreateChannelContainer();
-
     if (ngForm.submitted && ngForm.form.valid) {
-      this.memberService.setChannelData(this.newChannelName, this.newChannelDescription);
+      this.memberService.setChannelData(
+        this.newChannelName,
+        this.newChannelDescription
+      );
+    }
+    if (!this.isMobile) {
+      this.closeCreateChannelContainer();
     }
   }
 }
