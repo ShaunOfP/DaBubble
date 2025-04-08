2876823137d7e1945a3dcea8dd89191effaf7eff
ShaunOfP <131476113+ShaunOfP@users.noreply.github.com>
2025-04-08 19:36:05 +0200

    fixed bug with mouseenter event called repeatedly when cursor entered the reaction bubble

--- dabubble/src/app/components/general-view/chat/public-chat/reactions/reactions.component.ts
+++ dabubble/src/app/components/general-view/chat/public-chat/reactions/reactions.component.ts
@@ -12,81 +12,85 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './reactions.component.scss'
 })
 export class ReactionsComponent {
-  @Input() message!: any; 
-  @Input()  hoveredMessageId!: string | null
+  @Input() message!: any;
+  @Input() hoveredMessageId!: string | null
   showPopoverReaction: number | null = null;
   reactionUserNamesCache: { [key: number]: string[] } = {};
+  reactionEntriesList: { isImage: boolean; value: string; count: number; users: string[] }[]  = [];
 
   constructor(
     private userDataService: UserDatasService,
     private route: ActivatedRoute,
-  ){}
+  ) { }
 
 
-   reactionEntries(message: Message): {
-      isImage: boolean;
-      value: string;
-      count: number;
-      users: string[];
-    }[] {
-      return Object.entries(message.reaction || {}).map(([emoji, users]) => {
-        if (emoji === 'green_check') {
-          return {
-            isImage: true,
-            value: 'img/general-view/chat/green_check.svg',
-            count: (users as string[]).length,
-            users: users as string[],
-          };
-        } else {
-           return {
-            isImage: false,
-            value: emoji,
-            count: (users as string[]).length,
-            users: users as string[],
-          };
-        }
-      });
-    }
-  
-  
-    async showPopover(index: number, users: string[]) {
-      console.log(this.hoveredMessageId);
-      
-      if (!this.reactionUserNamesCache[index]) {
-        this.reactionUserNamesCache[index] = await this.formatUserNames(users);
+  ngOnChanges(){
+    this.reactionEntriesList = this.createReactionEntries(this.message);
+  }
+
+
+  createReactionEntries(message: Message): {
+    isImage: boolean;
+    value: string;
+    count: number;
+    users: string[];
+  }[] {
+    return Object.entries(message.reaction || {}).map(([emoji, users]) => {
+      if (emoji === 'green_check') {
+        return {
+          isImage: true,
+          value: 'img/general-view/chat/green_check.svg',
+          count: (users as string[]).length,
+          users: users as string[],
+        };
+      } else {
+        return {
+          isImage: false,
+          value: emoji,
+          count: (users as string[]).length,
+          users: users as string[],
+        };
       }
-      this.showPopoverReaction = index;      
-    }
-  
-    hidePopover() {
-      this.showPopoverReaction = null;
-    }
+    });
+  }
 
-    async formatUserNames(users: string[]): Promise<string[]> {
-      let formattedNames = await Promise.all(
-        users.map(async (id) => id === this.userDataService.currentUserId ? "Du" : await this.userDataService.getUserName(id))
-      );
-      const yourself = formattedNames.includes("Du");
-      formattedNames = formattedNames.filter(name => name !== "Du");
-      let maxNames = yourself ? 1 : 2;
-      let result = formattedNames.slice(0, maxNames);
-      if (yourself) {
-        result.push("Du");
-      }
-      return result;
-    }
 
-    checkStyle(userId: string): string {
-      let currentUser: string = '';
-      this.route.queryParams.subscribe((params) => {
-        currentUser = params['userID'];
-      });
-      return userId === currentUser ? 'secondary' : 'primary';
+  async showPopover(index: number, users: string[]) {
+    if (!this.reactionUserNamesCache[index]) {
+      this.reactionUserNamesCache[index] = await this.formatUserNames(users);
     }
-    
-    updateReaction(emoji: string){
-      console.log(emoji);
-      
+    this.showPopoverReaction = index;
+  }
+
+  hidePopover() {
+    this.showPopoverReaction = null;
+  }
+
+  async formatUserNames(users: string[]): Promise<string[]> {
+    let formattedNames = await Promise.all(
+      users.map(async (id) => id === this.userDataService.currentUserId ? "Du" : await this.userDataService.getUserName(id))
+    );
+    const yourself = formattedNames.includes("Du");
+    formattedNames = formattedNames.filter(name => name !== "Du");
+    let maxNames = yourself ? 1 : 2;
+    let result = formattedNames.slice(0, maxNames);
+    if (yourself) {
+      result.push("Du");
     }
+    return result;
+  }
+
+  checkStyle(userId: string): string {
+    let currentUser: string = '';
+    this.route.queryParams.subscribe((params) => {
+      currentUser = params['userID'];
+    });
+    return userId === currentUser ? 'secondary' : 'primary';
+  }
+
+  updateReaction(emoji: string) {
+    console.log(emoji);
+
+  }
 
 }
