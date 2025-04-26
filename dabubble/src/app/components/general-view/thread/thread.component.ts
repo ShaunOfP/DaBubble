84c352823bf2d0d04ca64dfabc9ead9be27c2058
ShaunOfP <131476113+ShaunOfP@users.noreply.github.com>
2025-04-26 21:00:06 +0200

    fixed thread styling and cleared textarea after message is send

--- dabubble/src/app/components/general-view/thread/thread.component.ts
+++ dabubble/src/app/components/general-view/thread/thread.component.ts
@@ -15,19 +15,19 @@ import { Message } from '../../../models/interfaces';
   templateUrl: './thread.component.html',
   styleUrl: './thread.component.scss'
 })
-export class ThreadComponent implements OnInit{
+export class ThreadComponent implements OnInit {
   @Output() callParent: EventEmitter<void> = new EventEmitter();
   @ViewChild('emojiTarget', { static: true }) emojiTarget!: ElementRef;
   selectedEmoji: string = '';
   toggleMarginLeft: boolean = true;
 
   constructor(public chatService: ChatService,
-  private userDatasService: UserDatasService) {
+    private userDatasService: UserDatasService) {
 
   }
 
   ngOnInit(): void {
-      
+
   }
 
   toggleMargin() {
@@ -56,27 +56,26 @@ export class ThreadComponent implements OnInit{
     }
   }
 
-  async sendMessage(content:string){
-  console.log(this.chatService.currentMessageId);
- 
-    if(!content) return
+  async sendMessage(content: string) {
+    if (!content) return
     const userId = this.userDatasService.currentUserId;
     const userName = await this.userDatasService.getUserName(userId);
     const avatarUrl = await this.userDatasService.getUserAvatar(this.userDatasService.currentUserId);
-    const message:  Message = {
-          id: this.generateId(), 
-          sender: userName, 
-          createdAt: new Date().getTime(),
-          content: content,
-          userId: this.userDatasService.currentUserId, // Use the actual user ID
-          reaction: {},
-          avatar: avatarUrl
-        };
+    const message: Message = {
+      id: this.generateId(),
+      sender: userName,
+      createdAt: new Date().getTime(),
+      content: content,
+      userId: this.userDatasService.currentUserId,
+      reaction: {},
+      avatar: avatarUrl
+    };
     const messageId = this.chatService.currentMessageId;
     this.chatService.generateThread(messageId, message);
+    this.emojiTarget.nativeElement.value = ``;
   }
+
   private generateId(): string {
-    // Generate a unique ID for the message (e.g., using a UUID library or custom logic)
-    return 'unique-id-' + Math.random().toString(36).substr(2, 9);
+    return 'unique-id-' + Math.random().toString(36).substring(2, 9);
   }
 }
