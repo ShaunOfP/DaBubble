65a5cbe5c18f39398833de3ccbf4c8bd811ee601
ShaunOfP <131476113+ShaunOfP@users.noreply.github.com>
2025-04-30 12:00:44 +0200

    added redirection when datas are empty

--- dabubble/src/app/components/landing-page/sign-in/avatar/avatar.component.ts
+++ dabubble/src/app/components/landing-page/sign-in/avatar/avatar.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormsModule, NgForm} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { Router, RouterModule } from '@angular/router';
 import { UserDatas } from '../../../../models/user.class';
 import { AuthService } from '../../../../services/firebase-services/auth.service';
@@ -13,107 +13,48 @@ import { FooterComponent } from "../../footer/footer.component";
 @Component({
   selector: 'app-avatar',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatCardModule, MatCheckboxModule, RouterModule, HeaderSectionComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatCardModule,
+    MatCheckboxModule,
+    RouterModule,
+    HeaderSectionComponent,
+    FooterComponent
+  ],
   templateUrl: './avatar.component.html',
   styleUrl: './avatar.component.scss'
 })
-export class AvatarComponent /* implements OnInit */ {
-/*   selectedAvatar: string | null = null; */
- /*  selectedAvatar: string = 'default-avatar'; */
+export class AvatarComponent {
   avatarCreated: boolean = false;
+  accountData!: UserDatas;
+  avatarList: Array<string> = ['avatar1', 'avatar2', 'avatar3', 'avatar4', 'avatar5', 'avatar6'];
 
-  public accountData!: UserDatas;
-
-  constructor(private router: Router, private authService : AuthService) {
-    try{
+  constructor(private router: Router, private authService: AuthService) {
     const navigation = this.router.getCurrentNavigation();
-    this.accountData = navigation?.extras?.state?.['accountData'];
-    console.log('User in AvatarComponent:', this.accountData);
-    }
-    catch(err){
-    this.checkInput()
+    if (navigation != null) {
+      this.accountData = navigation?.extras?.state?.['accountData'];
+    } else {
+      this.navigateTo('sign-in');
     }
   }
-  
-  avatarList: Array<string> = ['avatar1','avatar2','avatar3','avatar4','avatar5','avatar6']
 
   selectAvatar(avatar: string, event: Event): void {
     event.preventDefault();
     this.accountData.avatar = '/img/general-view/create-avatar/' + avatar + '.svg';
   }
 
-  createUser(ngForm:NgForm){
-    if(ngForm.valid && ngForm.submitted){
-      this.authService.createUserWithEmail(this.accountData)
+  createUser(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+      this.authService.createUserWithEmail(this.accountData);
       this.avatarCreated = true;
-      setTimeout(() => {        
+      setTimeout(() => {
         this.navigateTo('/')
       }, 1000);
-
     }
   }
 
   navigateTo(route: string) {
     this.router.navigate([route]);
   }
-
-  checkInput(){
-    if(this.accountData == undefined) this.navigateTo('sign-in')
-    return
-  }
 }
-
-
- /*  constructor(private router: Router, private userService: UserDatasService,) {} */
-/*   avatarList = [
-    {
-      name: 'avatar1',
-      img: '/img/general-view/create-avatar/avatar1.svg'
-    },
-    {
-      name: 'avatar2',
-      img: '/img/general-view/create-avatar/avatar2.svg'
-    },
-    {
-      name: 'avatar3',
-      img: '/img/general-view/create-avatar/avatar3.svg'
-    },
-    {
-      name: 'avatar4',
-      img: '/img/general-view/create-avatar/avatar4.svg'
-    },
-    {
-      name: 'avatar5',
-      img: '/img/general-view/create-avatar/avatar5.svg'
-    },
-    {
-      name: 'avatar6',
-      img: '/img/general-view/create-avatar/avatar6.svg'
-    },
-  ] */
-
- /*  ngOnInit(){
-    this.userService.user$.subscribe(user => {
-      if (user) {
-        this.user = user;
-      } else {
-        this.userService.getUserFromStorage();
-      }
-    });
-  }
- */
-
-
-  
- /*  createAvatar(){
-    if (this.selectedAvatar) {
-      this.userService.updateUserAvatar(this.user.id, this.selectedAvatarImg);
-      this.avatarCreated = true;
-      setTimeout(() => {
-        this.navigateTo('');
-        this.avatarCreated = false;
-        localStorage.removeItem('user')
-      }, 1300);
-    }
-  }
- */
